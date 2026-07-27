3171935ad3e161a40d685a91f371f66cdfa51cc8
Axedus-hue-spec <eduardgolovko40@gmail.com>
2026-07-27 09:03:27 +0300

    server commit

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ import cookieParser from "cookie-parser";
 import authMiddleware from "./middlewre/authMiddleware.js";
 import { userRouter } from "./routes/userRoutes.js";
 import { passwordRouter } from "./routes/passwordRoutes.js";
+import "dotenv/config"
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors({
@@ -14,11 +17,11 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/users", userRouter);
-app.use("/api/passwords", authMiddleware, passwordRouter)
+app.use("/api/passwords", authMiddleware, passwordRouter);
 
 export const db = new sqlite.Database("./password-manager.db", (err) => {
     if (err) console.log(err);
     else console.log("Подключено к БД");
-})
+});
 
-app.listen(3000, () => console.log('Сервер запущен'));
+app.listen(PORT, "0.0.0.0", () => console.log('Сервер запущен'));
